a9aaf6eca52e9b7cec323a210a5689fdf0541fa4
Johann <johann@immedia.co.za>
2013-02-26 13:23:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoPhotosRails::Application.config.secret_token = 'c3d35dc69064261bfd6d588121d97b4d66b6705623a1ace3efa0a64d261ae5740112ed301655c72d7cee9ee66c8f61eaf869a923f651cfba0d27925f1c8f6ede'
